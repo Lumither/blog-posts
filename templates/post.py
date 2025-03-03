002f4a07715066f16535ce293f2b4121d14d2aea
Lumither Tao <lumither@outlook.com>
2025-03-03 02:22:45 -0800

    feat(cli) update

--- templates/post.py
+++ templates/post.py
@@ -1,22 +1,70 @@
+import uuid
 from datetime import date
+from datetime import datetime as dt
 from typing import Dict, Optional
 
 from templates.utils import Template, create_all_files
 from utils.autocompletion import input_autocompletion, fuzzy_pinyin_matcher
+from utils.cache import get_tags_cache, get_categories_cache
+from utils.lang import eprint, wprint
 
 POST_TEMPLATE = '''
 ---
-title: { title }
-subtitle: { subtitle }
-published_date: { published_date }
-last_updated: { last_update }
-category: { category }
-tags: { tags }
+title: {{ title }}
+subtitle: {{ subtitle }}
+published_date: {{ published_date }}
+last_updated: {{ last_update }}
+category: {{ category }}
+tags: {{ tags }}
+uuid: {{ uuid }}
 ---
 
 WIP
 
 '''
+
+
+def get_title():
+    while True:
+        i = input("title > ")
+        if i.strip() == "":
+            eprint("empty title")
+            continue
+        else:
+            return i.strip()
+
+
+def get_subtitle():
+    return input("subtitle > ")
+
+
+def get_published_date():
+    while True:
+        d = input("published_date (YYYY-MM-DD) > ")
+        if d == "":
+            wprint("using today's date")
+            return dt.today().strftime("%Y-%m-%d")
+        else:
+            try:
+                _validate = dt.strptime(d, "%Y-%m-%d")
+                return d
+            except ValueError:
+                eprint("invalid date")
+                continue
+
+
+def get_tags():
+    return input_autocompletion("tags > ", get_tags_cache(), fuzzy_pinyin_matcher)
+
+
+def get_category():
+    while True:
+        i = input_autocompletion("category > ", get_categories_cache(), fuzzy_pinyin_matcher)
+        if i.strip() == "":
+            eprint("empty category")
+            continue
+        else:
+            return i.strip()
 
 
 class BlogPost(Template):
@@ -25,9 +73,14 @@ class BlogPost(Template):
 
     def init(self):
         year = date.today().year
-        print(input_autocompletion("input > ",
-                                   ["helllo", "测试", "你好", "这是一个非常有意思的故事", "test"],
-                                   fuzzy_pinyin_matcher
-                                   )
-              )
+
+        index_md = POST_TEMPLATE.format(
+            title=get_title(),
+            subtitle=get_subtitle(),
+            published_date=get_published_date(),
+            last_update=dt.today().strftime("%Y-%m-%d"),
+            tags=get_tags(),
+            category=get_category(),
+            uuid=uuid.uuid4()
+        )
         create_all_files(self.WORK_PATH.format(year=year, identifier="test"), self.ITEMS)
